7d09a9a1f1d713c3819ba7aaab06de2dbce032fc
Serhii_Y <dj.ser961@gmail.com>
2020-01-27 05:37:36 +0200

    Updates

---
+++ precache-manifest.72607634eb7539e8b0eab580b9c9e80c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ec188235f2816f7c686d709269671e",
+    "url": "/test-person-form/index.html"
+  },
+  {
+    "revision": "c93e3ca57c489c565b0e",
+    "url": "/test-person-form/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "33e9427651adefb7305e",
+    "url": "/test-person-form/static/css/main.c7234829.chunk.css"
+  },
+  {
+    "revision": "c93e3ca57c489c565b0e",
+    "url": "/test-person-form/static/js/2.f4c290ba.chunk.js"
+  },
+  {
+    "revision": "5758d22cb36d407a783f7c9e89d380e9",
+    "url": "/test-person-form/static/js/2.f4c290ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33e9427651adefb7305e",
+    "url": "/test-person-form/static/js/main.22c2f6fb.chunk.js"
+  },
+  {
+    "revision": "a4849aca71f11fc0ceb4",
+    "url": "/test-person-form/static/js/runtime-main.1de41da5.js"
+  }
+]);
